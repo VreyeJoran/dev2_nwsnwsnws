160eb04085fc55e0a46c8bd1585d62525a7821d8
VreyeJoran <joravrey@student.arteveldehs.be>
2025-02-27 16:00:10 +0100

    fix

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,29 @@
-import routes from "./routes";
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import path from "path";
+import expressLayouts from "express-ejs-layouts";
+import routes from "./routes";
 
 const app: Application = express();
 const PORT : number = parseInt(<string>process.env.PORT, 10) || 3000;
 
-// Route om een HTML-pagina te tonen
-app.get("/", (req: Request, res: Response): void => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
-  });
+// EJS als template-engine instellen
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware voor layouts
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
 
 // Middleware om statische bestanden te serveren
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.listen(PORT, () => {
+// Middleware om formulierdata te verwerken
+app.use(express.urlencoded({ extended: true }));
+
+// Routes gebruiken
+app.use("/", routes);
+
+// Server starten
+app.listen(PORT, (): void => {
   console.log(`Server draait op http://localhost:${PORT}`);
 });
--- server/routes.ts
+++ server/routes.ts
@@ -1,13 +1,21 @@
-import express, { Request, Response } from "express";
-import path from "path";
+import express, { Request, Response, Router } from "express";
+import{ getNews, getNewsBySlug, addNews } from "./data/newsService"
 
-const router = express.Router();
+const router: Router = express.Router();
 
-/**
- * GET / - Laadt de homepagina
- */
+// Homepagina
 router.get("/", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  const news = getNews();
+
+  res.render("index", { title: "News", news: news });
+});
+
+// Details
+router.get("/news/:slug", (req: Request, res: Response): void => {
+  const slug: string = req.params.slug;
+  const newsSlug = getNewsBySlug(slug);
+
+  res.render("details", { title: "News article", slug: newsSlug });
 });
 
 export default router;

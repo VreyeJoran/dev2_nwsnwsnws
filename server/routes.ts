f5ad903626cf760a40f24f5028dce5a2d272244a
VreyeJoran <joravrey@student.arteveldehs.be>
2025-03-06 15:26:18 +0100

    using database

--- server/routes.ts
+++ server/routes.ts
@@ -1,21 +1,34 @@
 import express, { Request, Response, Router } from "express";
-import{ getNews, getNewsBySlug, addNews } from "./data/newsService"
+import { News, getAllNews, getNewsBySlug, addNewsItem } from "./services/newsService";
 
 const router: Router = express.Router();
 
 // Homepagina
-router.get("/", (req: Request, res: Response): void => {
-  const news = getNews();
+// router.get("/", (req: Request, res: Response): void => {
+//   const news = getNews();
 
-  res.render("index", { title: "News", news: news });
-});
+//   res.render("index", { title: "News", news: news });
+// });
 
 // Details
-router.get("/news/:slug", (req: Request, res: Response): void => {
-  const slug: string = req.params.slug;
-  const newsSlug = getNewsBySlug(slug);
+// router.get("/news/:slug", (req: Request, res: Response): void => {
+//   const slug: string = req.params.slug;
+//   const newsSlug = getNewsBySlug(slug);
 
-  res.render("details", { title: "News article", slug: newsSlug });
+//   res.render("details", { title: "News article", slug: newsSlug });
+// });
+
+router.get("/", async (req: Request, res: Response) => {
+  const news: News[] = await getAllNews();
+
+  res.render("index", { news, title: "Recent news" });
+});
+
+router.get("/news/:slug", async (req: Request, res: Response) => {
+  const slug: string = req.params.slug;
+  const newsSlug = await getNewsBySlug(slug);
+
+  res.render("news", { title: "News article", news: newsSlug[0] });
 });
 
 export default router;
